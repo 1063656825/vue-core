95e4c20114dbff0940bc7ee4856e2d39a3cab79d
1063656825 <1063656825@qq.com>
2026-02-24 22:12:11 +0800

    Refactor Rollup configuration and improve project structure
    - Updated Rollup configuration to enhance output options and support UMD format.
    - Cleaned up project structure by removing outdated files and improving organization.
    - Adjusted last edit timestamp for better change tracking.

---
+++ test/2-1.渲染器的基本设计/5.其他简单类型节点的处理/index.js
@@ -0,0 +1,351 @@
+const options = {
+    createText(text){
+        return document.createTextNode(text);
+    },
+    // 创建注释节点
+    createComment(text){
+        return document.createComment(text);
+    },
+    setText(el, text){
+        el.nodeValue = text;
+    },
+    createElement(tag) {
+        return document.createElement(tag);
+    },
+    setElementText(el, text) {
+        el.textContent = text;
+    },
+    insert(el, parent, anchor = null) {
+        parent.insertBefore(el, anchor);
+    },
+    patchProps(el, key, prevValue, nextValue) {
+        if (key?.startsWith('on')) {
+            const eventName = key.slice(2).toLowerCase();
+            // _vei vue event invoker的简写，就是一个缓存
+            let invokers = el._vei || (el._vei = {});
+            let invoker = invokers[key]
+            if (nextValue) {
+                if (!invoker) {
+                    invoker = el._vei = (e) => {
+                        if (e.timeStamp < invoker.attached) return;
+                        if (Array.isArray(invoker.value)) {
+                            invoker.value.forEach(fn => fn(e));
+                        } else {
+                            invoker.value(e);
+                        }
+                    }
+                    invoker.value = nextValue;
+                    invoker.attached = performance.now();
+                    el.addEventListener(eventName, nextValue);
+
+                } else {
+                    invoker.value = nextValue;
+                }
+            } else {
+                prevValue && el.removeEventListener(eventName, prevValue);
+
+            }
+        }
+        if (key === 'class') {
+            el.className = nextValue || ""
+        }
+        else if (shouldSetAsProps(el, key, nextValue)) {
+            const type = typeof el[key];
+            if (type === 'boolean' && nextValue === '') {
+                el[key] = true;
+            } else {
+                el[key] = nextValue;
+            }
+        } else {
+            el.setAttribute(key, nextValue);
+        }
+    }
+}
+
+function shouldSetAsProps(el, key, value) {
+    if (key === 'form' && el.tagName !== 'INPUT') {
+        return false;
+    }
+    return key in el;
+}
+
+function createRenderer(options) {
+    const { 
+        createText,
+        createComment,
+        setText,
+        createElement, 
+        setElementText, 
+        insert, 
+        patchProps 
+    } = options
+
+    function unmount(vnode) {
+        if( vnode.type === Fragment){
+            vnode.children.forEach(child => unmount(child));
+            return
+        }
+        const parent = vnode.el.parentNode;
+        if (parent) {
+            parent.removeChild(vnode.el);
+        }
+    }
+    function render(vnode, container) {
+        // 调用patch方法
+        if (vnode === null && container.__vnode) {
+            unmount(container.__vnode)
+        } else if (vnode !== null) {
+            patch(container.__vnode, vnode, container, null);
+        }
+
+        container.__vnode = vnode;
+    }
+
+    function patchElement(oldVnode, newVnode) {
+        const el = newVnode.el = oldVnode.el,
+            oldProps = oldVnode.props || {},
+            newProps = newVnode.props || {};
+
+        // 更新属性
+        for (const key in newProps) {
+            if (oldProps[key] !== newProps[key]) {
+                patchProps(el, key, oldProps[key], newProps[key]);
+            }
+        }
+
+        // 如果不存在则删除属性
+        for (const key in oldProps) {
+            if (!(key in newProps)) {
+                patchProps(el, key, oldProps[key], null);
+            }
+        }
+
+        // 更新子节点
+        patchChildren(oldVnode, newVnode, el);
+
+    }
+
+    /**
+     * @description: 子节点处理
+     * @param {*} oldVnode
+     * @param {*} newVnode
+     * @param {*} container
+     * @return {*}
+     */
+    function patchChildren(oldVnode, newVnode, container) { 
+        // 子节点为字符串
+        if(typeof newVnopde.children === 'string' ){
+            if(Array.isArray(oldVnode.children)){
+                oldVnode.children.forEach(child => unmount(child))
+            }
+
+            setElementText(container, newVnode.children);
+
+        }else if(Array.isArray(newVnode.children)){
+            // 子节点为数组,循环卸载子节点
+            if(Array.isArray(oldVnode.children)){
+                oldVnode.children.forEach(child => unmount(child))
+            }else {
+                setElementText(container, '');
+            }
+ 
+            newVnode.children.forEach((child, index) => {
+                patch(null, child, container);
+            })
+
+        }else{
+            // 子节点为空节点 
+            if(Array.isArray(oldVnode.children)){
+                oldVnode.children.forEach(child => unmount(child))
+            }
+            setElementText(container, '');
+
+        }
+    }   
+
+    function patch(oldVnode, newVnode, container, parentComponent) {
+        if (oldVnode && oldVnode.type === newVnode.type) {
+            unmount(oldVnode);
+            oldVnode = null
+        }
+        const { type } = newVnode;
+        if(type === Text){
+            if (!oldVnode) {
+                const el = (newVnode.el = createText(newVnode.children));
+                // 插入文本节点
+                insert(el, container);
+              }
+              else { 
+                const el = (newVnode.el = oldVnode.el);
+                if(newVnode.children !== oldVnode.children) { 
+                  setText(el, newVnode.children);
+                }
+              }
+
+        }else if (type === Comment) { 
+            // 还是要区分有没有旧节点
+            // 如果没有旧节点，直接挂载
+            if (!oldVnode) {
+              const el = (newVnode.el = createComment(newVnode.children));
+              // 插入注释节点
+              insert(el, container);
+            }
+            else { 
+              const el = (newVnode.el = oldVnode.el);
+              if(newVnode.children !== oldVnode.children) { 
+                setText(el, newVnode.children);
+              }
+            }
+          }
+          else if (type === Fragment) { 
+            if (!oldVnode) {
+              newVnode.children.forEach((child) => {
+                patch(null, child, container);
+              })
+            }
+            else { 
+              // 如果oldVnode存在，其实就是更新子节点
+              patchChildren(oldVnode, newVnode, container);
+            }
+          }else if (typeof type === 'string') {
+            if (!oldVnode) {
+                // 挂载
+                mountElement(newVnode, container, parentComponent);
+            } else {
+                // 更新
+                patchElement(oldVnode, newVnode, container, parentComponent);
+            }
+        } else if (typeof type === 'object') {
+            // 组件
+        } else {
+            // 其他形式
+        }
+
+    }
+
+    function mountElement(vnode, container, parentComponent) {
+        const el = vnode.el = createElement(vnode.type);
+        // 遍历子节点，针对不同的子节点，进行处理
+        if (typeof vnode.children === 'string') {
+            setElementText(el, vnode.children)
+        } else if (Array.isArray(vnode.children)) {
+            vnode.children.forEach(child => {
+                patch(null, child, el)
+            })
+        }
+
+        /**
+         * 注意挂载属性的时候，需要注意的问题
+         * HTML标签属性名字和DOM API属性名字可能不一样，甚至有可能有些有，有些没有
+         * 
+         */
+
+        if (vnode.props) {
+            for (const key in vnode.props) {
+                const value = vnode.props[key];
+                patchProps(el, key, null, value)
+            }
+        }
+        container.append(el)
+    }
+
+    function patchElement(n1, n2, container, parentComponent) {
+
+    }
+
+    return {
+        render
+    };
+}
+
+
+const cls = { btn: true, 'btn-primary': true }
+const clsArr = ['btn', 'btn-primary', { baz: true }]
+
+const isString = (val) => {
+    return typeof val === 'string'
+}
+
+const isObject = (val) => {
+    return val !== null && typeof val === 'object'
+}
+
+const isArray = Array.isArray
+
+function normalizeClass(value) {
+    let res = "";
+
+    if (isString(value)) {
+        res = value;
+    }
+    else if (isArray(value)) {
+        for (let i = 0; i < value.length; i++) {
+            const normalized = normalizeClass(value[i])
+            if (normalized) {
+                res += normalized + " "
+            }
+        }
+    }
+    else if (isObject(value)) {
+        for (const name in value) {
+            if (value[name]) {
+                res += name + " "
+            }
+        }
+
+    }
+
+
+    return res;
+}
+
+
+
+const Text = Symbol("Text");
+const Comment = Symbol("Comment");
+const Fragment = Symbol("Fragment");
+
+// 文本节点和注释节点
+const vnode1 = {
+  type: Text,
+  children: "我是文本",
+}
+
+const vnode2 = {
+  type: Comment,
+  children: "我是注释",
+}
+
+const renderer = createRenderer(options);
+renderer.render(vnode1, document.getElementById("app"));
+// renderer.render(vnode2, document.getElementById("app"));
+
+// Fragment 文档片段
+// const vnode = {
+//   type: Fragment,
+//   children: [
+//     { type: "li", children: "text 1" },
+//     { type: "li", children: "text 2" },
+//     { type: "li", children: "text 3" },
+//     { type: "li", children: "text 4" }
+//   ]
+// }
+
+// const vnode = {
+//   type: 'ul',
+//   children: [
+//     {
+//       type: Fragment,
+//       children: [
+//         { type: "li", children: "text 1" },
+//         { type: "li", children: "text 2" },
+//         { type: "li", children: "text 3" },
+//         { type: "li", children: "text 4" }
+//       ]
+//     }
+//   ]
+  
+// }
+
+// const renderer = createRenderer(options);
+// renderer.render(vnode, document.getElementById("app"));
